6d2f5b388d53e8ba092242c7140575dea58a197b
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2024-09-23 10:45:31 +0700

    Fix vernam cipher

--- lab-2/vernam.go
+++ lab-2/vernam.go
@@ -1,31 +1,67 @@
 package main
 
 import (
-	"math/rand"
+	"crypto/rand"
+	"io/ioutil"
 )
 
-func vernamEncrypt(data []byte) []byte {
-	key := generateVernamKey(len(data))
-	return xorBytes(data, key)
-}
-
-func vernamDecrypt(data []byte) []byte {
-	key := generateVernamKey(len(data))
-	return xorBytes(data, key)
-}
-
-func generateVernamKey(length int) []byte {
+// Генерация ключа той же длины, что и данные
+func generateKey(length int) ([]byte, error) {
 	key := make([]byte, length)
-	for i := range key { // just random bytes
-		key[i] = byte(rand.Intn(256))
+	_, err := rand.Read(key)
+	if err != nil {
+		return nil, err
 	}
-	return key
+	return key, nil
 }
 
-func xorBytes(data, key []byte) []byte {
+// Сохранение ключа в файл
+func saveVernamKeyToFile(filename string, key []byte) error {
+	return ioutil.WriteFile(filename, key, 0644)
+}
+
+// Чтение ключа из файла
+func readVernamKeyFromFile(filename string) ([]byte, error) {
+	return ioutil.ReadFile(filename)
+}
+
+// Шифрование и расшифрование шифром Вернама
+func vernamEncryptDecrypt(data []byte, key []byte) []byte {
 	result := make([]byte, len(data))
-	for i := range data { // XOR for every byte
-		result[i] = data[i] ^ key[i%len(key)]
+	for i := range data {
+		result[i] = data[i] ^ key[i] // XOR между данными и ключом
 	}
 	return result
+}
+
+// Шифрование данных с использованием Вернама
+func vernamEncrypt(data []byte) ([]byte, error) {
+	// Генерируем ключ той же длины, что и данные
+	key, err := generateKey(len(data))
+	if err != nil {
+		return nil, err
+	}
+
+	// Сохраняем ключ в файл
+	err = saveVernamKeyToFile("vernam_key.txt", key)
+	if err != nil {
+		return nil, err
+	}
+
+	// Шифруем данные с помощью XOR
+	encryptedData := vernamEncryptDecrypt(data, key)
+	return encryptedData, nil
+}
+
+// Расшифрование данных с использованием Вернама
+func vernamDecrypt(data []byte) ([]byte, error) {
+	// Считываем ключ из файла
+	key, err := readVernamKeyFromFile("vernam_key.txt")
+	if err != nil {
+		return nil, err
+	}
+
+	// Расшифровываем данные с помощью XOR
+	decryptedData := vernamEncryptDecrypt(data, key)
+	return decryptedData, nil
 }
